f783c47b258b3e225aa39f7e4a6a3852bf475504
shinshin <preashane5@gmail.com>
2022-10-07 16:06:25 +0800

    [nav-tabs][DONE] - adding bootstrap.js

--- admin/certificate.php
+++ admin/certificate.php
@@ -21,6 +21,10 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> 
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
+ <!-- Nav tabs -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
   
 
