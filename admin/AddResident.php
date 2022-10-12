ae953de02f449f3e02bd61333b6bba59cdb1ae3c
shinshin <preashane5@gmail.com>
2022-10-12 18:07:51 +0800

    [AddResidentModal][WIP] - validation

--- admin/AddResident.php
+++ admin/AddResident.php
@@ -1,41 +1,46 @@
+<script type="text/javascript" src="../assets/js/validation.js"></script>
+
 <!-- addResidentModal -->
 <div id="addResidentModal" class="modal fade bd-example-modal-lg">
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
-			<form>
+			<form name="RegForm" method="post" action="" name="Vform" role="form" onsubmit="return validateForm()">   
 				<div class="modal-header">						
 					<h4 class="modal-title">Add New</h4>
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 				</div>
 				<div class="modal-body">
-				<div class="row gx-3 mb-3">					
-					<div class="col-md-4">
+				<div class="row gx-3 mb-3">	
+					<!-- dre boss-->
+					<div class="col-md-4" id="fname_div">
 						<label>First Name</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control" name="fname">
+						<div id="fname_error"></div>
 					</div>
+					<!-- dre boss-->
           			<div class="col-md-4">
 						<label>Middle Name</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
           			<div class="col-md-4">
 						<label>Last Name</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-6">
 						<label>Place of Birth</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-6">
 						<label>Barangay</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-4">
 						<label>Zone</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-4">
 						<label>Civil Status</label>
-						<select name="" class="form-control" required> 
+						<select name="" class="form-control"> 
 							<option disabled selected hidden></option>
 							<option>Single</option>
                             <option>Married</option>
@@ -46,11 +51,11 @@
 					</div>
 					<div class="col-md-4">
 						<label>Occupation</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-4">
 						<label>Age</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-4">
 						<label>Blood Type</label>
@@ -68,15 +73,15 @@
 					</div>
 					<div class="col-md-4">
 						<label>Nationality</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-4">
 						<label>No. of Household Member</label>
-						<input type="number" min="1" class="form-control" required>
+						<input type="number" min="1" class="form-control">
 					</div>
           			<div class="col-md-4">
 						<label>Sex</label>
-						<select name="" class="form-control" required> 
+						<select name="" class="form-control"> 
 								<option disabled selected hidden></option>
 								<option>Female</option>
                 				<option>Male</option>
@@ -84,7 +89,7 @@
 					</div>
 					<div class="col-md-4">
 						<label>Educational Attainment</label>
-						<select name="" class="form-control" required>
+						<select name="" class="form-control">
                                 <option disabled selected hidden></option>								        
 								<option>Not Applicable</option>
                                 <option>Elementary Level</option>
@@ -100,19 +105,19 @@
 					</div>		
 					<div class="col-md-4">
 						<label>Date of Birth</label>
-						<input type="date" class="form-control" required>
+						<input type="date" class="form-control">
 					</div>	
 					<div class="col-md-8">
 						<label>Photo (.jpg, .jpeg & .png)</label>
-						<input type="file" class="form-control" accept= ".png, .jpg, .jpeg" required>
+						<input type="file" class="form-control" accept= ".png, .jpg, .jpeg" >
 					</div>
 					<div class="col-md-6">
 						<label>Username</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 					<div class="col-md-6">
 						<label>Password</label>
-						<input type="text" class="form-control" required>
+						<input type="text" class="form-control">
 					</div>
 				</div>
 				</div>
@@ -124,3 +129,4 @@
 		</div>
 	</div>
 </div>
+
