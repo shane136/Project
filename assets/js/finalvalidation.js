4611308d0ae0f91091559d08a222859698b20200
nas@solutionsresource.com <nas@solutionsresource.com>
2022-10-13 19:42:04 +0800

    [Validation Refactored][WIP] - Refactored validationjs.

--- assets/js/finalvalidation.js
+++ assets/js/finalvalidation.js
@@ -36,20 +36,30 @@ function checkValidation(){
 
     //addEventListener input, gina check nya if nagsulat ka or typing dra nga field or naka focus dra nga field
     nameInput.addEventListener('input', function(){
-        if(nameInput.value.match(specialChar) && nameInput.value.length > 0){
-            var child = document.getElementById('firstName');
-            nameInput.value = nameInput.value.toUpperCase();
-            child.style.display = "none";
-            fname.style.border = "1px solid green";
+        if(nameInput.value.length > 0){
+            if(nameInput.value.match(specialChar)){
+                var child = document.getElementById('firstName');
+                nameInput.value = nameInput.value.toUpperCase();
+                child.style.display = "none";
+                fname.style.border = "1px solid green";
+            }
+            else{
+                var child = document.getElementById('firstName');
+                fname.style.border = "1px solid red";
+                child.style.display = "inline";
+                child.innerHTML = "Special char(s) are not allowed!";
+            }
+            
         }
         else{
             var child = document.getElementById('firstName');
             child.style.display = "inline"; 
             fname.style.border = "1px solid red";
+            child.innerHTML = "First Name is Required";
         }
     });
     mNameInput.addEventListener('input', function(){
-        if(mNameInput.value.length > 0 && nameInput.value.length > 0){
+        if(mNameInput.value.length > 0 && mNameInput.value.match(specialChar)){
             var child = document.getElementById('midName');
             mNameInput.value = mNameInput.value.toUpperCase();
             child.style.display = "none";
